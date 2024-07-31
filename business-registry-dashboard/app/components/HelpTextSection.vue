e0897f68f69dd72fc414352c06edddd015b8a425
deetz99 <dietrich@wolpert.ca>
2024-07-31 11:25:50 -0700

    add ally attrs to help text btn

--- business-registry-dashboard/app/components/HelpTextSection.vue
+++ business-registry-dashboard/app/components/HelpTextSection.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+import { UInput } from '#components'
 const { locale } = useI18n()
 const showHelpText = ref(false)
+const showHelpTextBtnRef = ref<InstanceType<typeof UInput> | null>(null)
+
+const toggleHelpText = () => {
+  showHelpText.value = !showHelpText.value
+  if (!showHelpText.value) {
+    showHelpTextBtnRef.value?.$el.focus()
+    showHelpTextBtnRef.value?.$el.scrollIntoView({ block: 'center' })
+  }
+}
 
 const { data: helpText } = await useAsyncData('start-manage-business-help-text', () => {
   return queryContent()
@@ -10,14 +20,19 @@ const { data: helpText } = await useAsyncData('start-manage-business-help-text',
 </script>
 <template>
   <UButton
+    ref="showHelpTextBtnRef"
     :label="showHelpText ? $t('btn.busStartHelp.hide') : $t('btn.busStartHelp.show')"
     variant="link"
     icon="i-mdi-help-circle-outline"
     class="max-w-fit"
+    :aria-expanded="showHelpText"
+    aria-controls="help-text-content"
     :ui="{ icon: { size: { sm: 'size-6' } } }"
-    @click="showHelpText = !showHelpText"
+    @click="toggleHelpText"
   />
   <div
+    id="help-text-content"
+    role="region"
     class="mx-auto min-w-[75vw] max-w-screen-lg overflow-hidden border-y border-dashed border-gray-700 transition-all duration-500 ease-in-out"
     :class="{
       '-mb-3 max-h-0 opacity-0': !showHelpText,
@@ -30,7 +45,7 @@ const { data: helpText } = await useAsyncData('start-manage-business-help-text',
         :label="$t('btn.busStartHelp.hide')"
         variant="link"
         class="ml-auto"
-        @click="showHelpText = false"
+        @click="toggleHelpText"
       />
     </div>
   </div>
