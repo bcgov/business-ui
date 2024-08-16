9f8fbdb9fce0305822fe6c24272d69aecdfb180c
deetz99 <dietrich@wolpert.ca>
2024-08-16 15:26:54 -0700

    add back in util after cherry pick

---
+++ business-registry-dashboard/app/utils/namex-search.ts
@@ -0,0 +1,18 @@
+export async function namexSearch (query: string): Promise<NamexResult[]> {
+  const keycloak = useKeycloak()
+  const namexApiUrl = useRuntimeConfig().public.namexApiUrl
+
+  const token = await keycloak.getToken()
+  const response = await $fetch<NamexResult[]>(namexApiUrl + '/requests/search', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    params: {
+      query,
+      start: 0,
+      rows: 20
+    }
+  })
+
+  return response
+}
