15911575295975db3a2e3e436d37b688275e505b
deetz99 <dietrich@wolpert.ca>
2024-08-16 14:09:53 -0700

    test reg search util

---
+++ business-registry-dashboard/tests/unit/utils/reg-search.test.ts
@@ -0,0 +1,73 @@
+import { describe, expect, it, vi, afterEach } from 'vitest'
+import { mockNuxtImport } from '@nuxt/test-utils/runtime'
+import { flushPromises } from '@vue/test-utils'
+
+mockNuxtImport('useKeycloak', () => {
+  return () => (
+    {
+      getToken: vi.fn().mockResolvedValue('123')
+    }
+  )
+})
+
+mockNuxtImport('useConnectAccountStore', () => {
+  return () => (
+    {
+      currentAccount: {
+        id: 1
+      }
+    }
+  )
+})
+
+mockNuxtImport('useRuntimeConfig', () => {
+  return () => (
+    {
+      public: {
+        regSearchApiUrl: 'https://regSearchApiUrl.example.com',
+        xApiKey: 'xApiKey.example'
+      }
+    }
+  )
+})
+
+describe('regSearch', () => {
+  afterEach(() => {
+    vi.unstubAllGlobals()
+  })
+
+  it('should make a fetch request with the correct options', async () => {
+    const _fetch = vi.fn().mockResolvedValue({ searchResults: { results: [] } })
+    vi.stubGlobal('$fetch', _fetch)
+
+    const queryStr = 'test'
+    await regSearch(queryStr)
+
+    await flushPromises()
+
+    expect(_fetch).toHaveBeenCalledWith(
+      'https://regSearchApiUrl.example.com/search/businesses',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer 123',
+          'x-apikey': 'xApiKey.example',
+          'Account-Id': 1
+        },
+        body: {
+          query: { value: queryStr },
+          categories: {
+            status: ['ACTIVE'],
+            legalType: [
+              'A', 'BC', 'BEN', 'C', 'CBEN', 'CC', 'CCC', 'CP', 'CUL', 'FI',
+              'GP', 'LL', 'LLC', 'LP', 'PA', 'S', 'SP', 'ULC', 'XCP', 'XL',
+              'XP', 'XS'
+            ]
+          },
+          rows: 20,
+          start: 0
+        }
+      }
+    )
+  })
+})
