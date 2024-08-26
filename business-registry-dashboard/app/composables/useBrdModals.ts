9f47e066dcfbfbf7733b4aed3d6a4639e67bd45b
deetz99 <dietrich@wolpert.ca>
2024-08-26 15:48:42 -0700

    add nr error function in usemodal wrapper

--- business-registry-dashboard/app/composables/useBrdModals.ts
+++ business-registry-dashboard/app/composables/useBrdModals.ts
@@ -1,12 +1,23 @@
 // https://ui.nuxt.com/components/modal#control-programmatically
-import { ModalManageNameRequest } from '#components'
+import { ModalManageNameRequest, ModalBase } from '#components'
 
 export const useBrdModals = () => {
   const modal = useModal()
+  const { t } = useI18n()
 
   function manageNameRequest (nr: { names: string[], nrNum: string }) {
     modal.open(ModalManageNameRequest, {
       nameRequest: nr
+    })
+  }
+
+  function manageNRError () {
+    modal.open(ModalBase, {
+      error: {
+        title: t('form.manageNR.error.default.title'),
+        description: t('form.manageNR.error.default.description')
+      },
+      actions: [{ label: t('btn.close'), handler: () => close() }]
     })
   }
 
@@ -16,6 +27,7 @@ export const useBrdModals = () => {
 
   return {
     manageNameRequest,
+    manageNRError,
     close
   }
 }
