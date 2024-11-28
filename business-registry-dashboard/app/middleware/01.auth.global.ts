371bf740dffe0e25ef717c1ddd5b6b2ee8e4b626
JazzarKarim <karim.jazzar@gov.bc.ca>
2024-11-28 15:38:54 -0800

    error fix

--- business-registry-dashboard/app/middleware/01.auth.global.ts
+++ business-registry-dashboard/app/middleware/01.auth.global.ts
@@ -2,7 +2,6 @@ export default defineNuxtRouteMiddleware((to) => {
   console.log('to', to)
   console.log('Route meta:', to.meta)
   console.log('Route params:', to.params)
-  console.log('Authentication status:', useNuxtApp().$keycloak.authenticated)
 
   if (process.client) {
     const { $keycloak } = useNuxtApp()
