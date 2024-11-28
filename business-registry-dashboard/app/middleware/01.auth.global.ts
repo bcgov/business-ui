605667bb3854914f92e3483284ad2851d5ad16e9
JazzarKarim <karim.jazzar@gov.bc.ca>
2024-11-28 15:47:21 -0800

    removed auth middleware

--- business-registry-dashboard/app/middleware/01.auth.global.ts
+++ business-registry-dashboard/app/middleware/01.auth.global.ts
@@ -3,11 +3,11 @@ export default defineNuxtRouteMiddleware((to) => {
   console.log('Route meta:', to.meta)
   console.log('Route params:', to.params)
 
-  if (process.client) {
-    const { $keycloak } = useNuxtApp()
-    const localePath = useLocalePath()
-    if (to.meta.order !== 0 && !$keycloak.authenticated) {
-      return navigateTo(localePath('/'))
-    }
-  }
+  // if (process.client) {
+  //   const { $keycloak } = useNuxtApp()
+  //   const localePath = useLocalePath()
+  //   if (to.meta.order !== 0 && !$keycloak.authenticated) {
+  //     return navigateTo(localePath('/'))
+  //   }
+  // }
 })
