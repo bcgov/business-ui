ad985f43a1924254184c3e1761d5c9466f972427
JazzarKarim <karim.jazzar@gov.bc.ca>
2024-11-28 15:32:32 -0800

    added some debug code

--- business-registry-dashboard/app/middleware/01.auth.global.ts
+++ business-registry-dashboard/app/middleware/01.auth.global.ts
@@ -1,4 +1,9 @@
 export default defineNuxtRouteMiddleware((to) => {
+  console.log('to', to)
+  console.log('Route meta:', to.meta)
+  console.log('Route params:', to.params)
+  console.log('Authentication status:', useNuxtApp().$keycloak.authenticated)
+
   if (process.client) {
     const { $keycloak } = useNuxtApp()
     const localePath = useLocalePath()
