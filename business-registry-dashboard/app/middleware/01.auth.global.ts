db476ead66895ba736e804d4607986dbfd373158
JazzarKarim <karim.jazzar@gov.bc.ca>
2024-12-02 14:32:10 -0800

    changed auth middleware to deal with token conditionally

--- business-registry-dashboard/app/middleware/01.auth.global.ts
+++ business-registry-dashboard/app/middleware/01.auth.global.ts
@@ -2,8 +2,17 @@ export default defineNuxtRouteMiddleware((to) => {
   if (process.client) {
     const { $keycloak } = useNuxtApp()
     const localePath = useLocalePath()
-    if (to.meta.order !== 0 && !$keycloak.authenticated) {
+
+    // If it's an invitation acceptance URL, handle it differently
+    if (to.path.includes('/affiliationInvitation/acceptToken/')) {
+      // Option 1: Force reload
       window.location.href = localePath('/')
+      return
+    }
+
+    // Regular auth check
+    if (to.meta.order !== 0 && !$keycloak.authenticated) {
+      return navigateTo(localePath('/'))
     }
   }
 })
