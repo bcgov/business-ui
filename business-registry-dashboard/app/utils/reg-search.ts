c4281350a57c20841fa5318098f9c62d8a18e4f0
karim.jazzar@gov.bc.ca <karim.jazzar@gov.bc.ca>
2025-01-03 14:38:13 -0500

    fix: moved to use v1 search endpoint

--- business-registry-dashboard/app/utils/reg-search.ts
+++ business-registry-dashboard/app/utils/reg-search.ts
@@ -4,23 +4,16 @@ export async function regSearch (queryStr: string): Promise<RegSearchResult[]> {
   const accountStore = useConnectAccountStore()
   const token = await keycloak.getToken()
 
-  const response = await $fetch<RegSearchResponse>(`${config.regSearchApiUrl}/search/businesses`, {
-    method: 'POST',
+  const legalType = 'A,BC,BEN,C,CBEN,CC,CCC,CP,CUL,FI,GP,LL,LLC,LP,PA,S,SP,ULC,XCP,XL,XP,XS'
+  let url = `${config.regSearchApiUrl}/businesses/search/facets?start=0&rows=20`
+  url += `&categories=legalType:${legalType}`
+  url += `&query=value:${encodeURIComponent(queryStr)}`
+
+  const response = await $fetch<RegSearchResponse>(url, {
     headers: {
       Authorization: `Bearer ${token}`,
       'x-apikey': config.xApiKey,
       'Account-Id': accountStore.currentAccount.id
-    },
-    body: {
-      query: {
-        value: queryStr
-      },
-      categories: {
-        status: ['ACTIVE'],
-        legalType: ['A', 'BC', 'BEN', 'C', 'CBEN', 'CC', 'CCC', 'CP', 'CUL', 'FI', 'GP', 'LL', 'LLC', 'LP', 'PA', 'S', 'SP', 'ULC', 'XCP', 'XL', 'XP', 'XS']
-      },
-      rows: 20,
-      start: 0
     }
   })
 
