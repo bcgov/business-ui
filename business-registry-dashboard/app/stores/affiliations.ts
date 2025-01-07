64e7d540398b9a6bde398cfe733493a5284ca947
karim.jazzar@gov.bc.ca <karim.jazzar@gov.bc.ca>
2025-01-07 17:20:38 -0500

    fixed isStaffOrSbcStaff check

--- business-registry-dashboard/app/stores/affiliations.ts
+++ business-registry-dashboard/app/stores/affiliations.ts
@@ -139,7 +139,7 @@ export const useAffiliationsStore = defineStore('brd-affiliations-store', () => 
 
       const route = useRoute()
       // Use route param if staff, otherwise use current account
-      const orgId = (isStaffOrSbcStaff && route.params.orgId)
+      const orgId = (accountStore.isStaffOrSbcStaff && route.params.orgId)
         ? route.params.orgId
         : accountStore.currentAccount.id
 
