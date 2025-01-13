9a840bfe68a73a925624a14d7dbeea47a917a8ae
Thayne Werdal <thayne.werdal@gov.bc.ca>
2025-01-13 08:38:36 -0800

    24102 - update to wording used on button options.

--- business-registry-dashboard/app/enums/affiliation-types.ts
+++ business-registry-dashboard/app/enums/affiliation-types.ts
@@ -1,7 +1,7 @@
 export enum AffiliationTypes {
   NAME_REQUEST = 'Name Request',
   AMALGAMATION_APPLICATION = 'Amalgamation Application',
-  CONTINUATION_APPLICATION = 'Continuation Application',
+  CONTINUATION_APPLICATION = 'Continuation Authorization',
   INCORPORATION_APPLICATION = 'Incorporation Application',
   CORPORATION = 'Corporation',
   REGISTRATION = 'Registration'
