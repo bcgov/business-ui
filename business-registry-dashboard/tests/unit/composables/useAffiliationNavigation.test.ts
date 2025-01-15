f6f81e290b14f507c2b9a10b191e2b0fca3bcf8f
Severin Beauvais <severin.beauvais@gov.bc.ca>
2025-01-15 12:39:13 -0800

    wip

--- business-registry-dashboard/tests/unit/composables/useAffiliationNavigation.test.ts
+++ business-registry-dashboard/tests/unit/composables/useAffiliationNavigation.test.ts
@@ -81,7 +81,7 @@ describe('useAffiliationNavigation', () => {
 
     expect(sessionStorage.setItem).toHaveBeenCalledWith('BUSINESS_ID', 'BC1234567')
     expect(navigateToMock).toHaveBeenCalledWith(
-      'https://business.example-dash.com/BC1234567?accountid=123',
+      'https://business-dash.example.com/BC1234567?accountid=123',
       { external: true }
     )
   })
