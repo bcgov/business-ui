fca478e0135a0dcdb38d8529c1c5eb641d1ee080
Karim El Jazzar <122301442+JazzarKarim@users.noreply.github.com>
2025-02-10 15:31:50 -0800

    fix: fixed primary action button width in response to UX notes (#66)
    * fix: fixed primary action button width in response to UX notes
    
    * fixed in response to Sev's comment
    
    * fixed lint

--- business-registry-dashboard/tailwind.config.ts
+++ business-registry-dashboard/tailwind.config.ts
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['**.{html,ts,js,vue}'],
-  presets: [require('@daxiom/nuxt-core-layer-test/tailwind.config')]
+  presets: [require('@daxiom/nuxt-core-layer-test/tailwind.config')],
+  theme: {
+    extend: {
+      width: {
+        45: '11.25rem'
+      }
+    }
+  }
 }
