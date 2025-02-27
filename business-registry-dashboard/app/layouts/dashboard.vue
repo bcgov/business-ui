97d43ec4e76049fecf7e0db3e3e21591e0b56311
JazzarKarim <karim.jazzar@gov.bc.ca>
2025-02-27 15:03:02 -0800

    fixed in response to Sev's comment

--- business-registry-dashboard/app/layouts/dashboard.vue
+++ business-registry-dashboard/app/layouts/dashboard.vue
@@ -48,7 +48,9 @@ onMounted(() => {
   }
   // Redirect inactive accounts to the account settings page
   // This ensures users can't access the dashboard until their account is activated
-  if (accountStore.currentAccount?.id &&
+  // We check for valid IDs (>= 0) to ensure we only redirect accounts that exist in the system
+  if (accountStore.currentAccount &&
+      Number(accountStore.currentAccount.id) >= 0 &&
       accountStore.currentAccount.accountStatus !== AccountStatus.ACTIVE) {
     const accountId = accountStore.currentAccount.id
     window.location.href = `${config.authWebUrl}/account/${accountId}/settings/account-info`
