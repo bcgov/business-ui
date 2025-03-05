730c4a05146ca48cc308319b5805a69090bc0fb9
JazzarKarim <karim.jazzar@gov.bc.ca>
2025-03-05 12:59:13 -0800

    feat: magic link routes for Continuation In and Amalgamation NRs + refactoring

--- business-registry-dashboard/app/locales/fr-CA.ts
+++ business-registry-dashboard/app/locales/fr-CA.ts
@@ -169,7 +169,7 @@ export default {
       title: 'Entreprise Déjà Ajoutée',
       description: 'L\'entreprise {identifier} est déjà dans votre liste du Registre des entreprises.'
     },
-    magicLinkIncorporateNowError: {
+    magicLinkFilingError: {
       title: 'Demande de nom indisponible ou problèmes de lien',
       description: "Confirmez que vous n'avez pas déjà utilisé ce nom pour une autre entreprise. Si le nom n'a pas été utilisé, veuillez vérifier dans quelques minutes la dernière mise à jour, car le système est peut-être encore en cours de traitement.",
       description2: "Assurez-vous d'avoir saisi le lien correctement, car des erreurs dans le lien peuvent provoquer l'apparition de ce message."
