3a950e25623278e9a1256145dd7cac367a193b60
deetz99 <dietrich@wolpert.ca>
2025-03-27 12:45:03 -0700

    init person-roles and web directory

---
+++ web/person-roles/i18n/locales/en-CA.ts
@@ -0,0 +1,9 @@
+export default {
+  // components
+  ConnectHeader: {
+    title: 'BC Registries and Online Services'
+  },
+  ConnectBreadcrumb: {
+    default: 'BC Registries and Online Services'
+  }
+}
