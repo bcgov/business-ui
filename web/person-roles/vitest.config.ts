4822b5f99c8a3ace159ee49c18c2879117b71f58
deetz99 <dietrich@wolpert.ca>
2025-03-28 11:05:50 -0700

    add vitest coverage

--- web/person-roles/vitest.config.ts
+++ web/person-roles/vitest.config.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath } from 'node:url'
-import path from 'path'
 import { createResolver } from 'nuxt/kit'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
@@ -9,20 +7,20 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     dir: 'tests',
-    // coverage: {
-    //   provider: 'v8',
-    //   reportsDirectory: path.resolve(__dirname, 'tests/coverage'), // This ensures an absolute path,
-    //   include: [
-    //     'pages/**',
-    //     'layouts/**',
-    //     'components/**',
-    //     'composables/**',
-    //     'utils/**',
-    //     'services/**',
-    //     'plugins/**',
-    //     'stores/**'
-    //   ]
-    // },
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: resolve('./tests/unit/coverage'), // This ensures an absolute path,
+      include: [
+        'pages/**',
+        'layouts/**',
+        'components/**',
+        'composables/**',
+        'utils/**',
+        'services/**',
+        'plugins/**',
+        'stores/**'
+      ]
+    },
     environmentOptions: {
       nuxt: {
         rootDir: resolve('./'),
