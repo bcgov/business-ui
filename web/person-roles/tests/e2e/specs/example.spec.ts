9c3da26ae0949ca1878b9a4e468cb7b7d2e6bae1
deetz99 <dietrich@wolpert.ca>
2025-03-28 11:36:32 -0700

    add accessibility check

--- web/person-roles/tests/e2e/specs/example.spec.ts
+++ web/person-roles/tests/e2e/specs/example.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test'
+import { scanA11y } from '../test-utils/a11y'
 
 test.describe('Example', () => {
   test('Passes', async ({ page }) => {
     await page.goto('./')
     await expect(page.getByText('TBD')).toBeVisible()
+
+    await scanA11y(page)
   })
 })
