3eb4ca343d3c091eafb7b68c04846f1f5730e40a
Karim El Jazzar <karim.jazzar@gov.bc.ca>
2025-04-09 15:29:20 -0400

    updated in response to Sev's comment

--- business-registry-dashboard/app/enums/display-name-to-corp-type.ts
+++ business-registry-dashboard/app/enums/display-name-to-corp-type.ts
@@ -1,17 +1,23 @@
+import { CorpTypeCd, GetCorpFullDescription } from '@bcrs-shared-components/corp-type-module'
+
 /**
  * Maps display names (filter values in table) to their corresponding corp type codes.
+ * This is essentially a reverse mapping of GetCorpFullDescription + special cases.
  */
-export enum DisplayNameToCorpType {
-  'BC Limited Company' = CorpTypes.BC_COMPANY,
-  'Benefit Company' = CorpTypes.BENEFIT_COMPANY,
-  'BC Community Contribution Company' = CorpTypes.BC_CCC,
-  'BC Unlimited Liability Company' = CorpTypes.BC_ULC_COMPANY,
-  'BC Cooperative Association' = CorpTypes.COOP,
-  'BC General Partnership' = CorpTypes.PARTNERSHIP,
-  'BC Sole Proprietorship' = CorpTypes.SOLE_PROP,
-  'Name Request' = CorpTypes.NAME_REQUEST,
-  'Incorporation Application' = CorpTypes.INCORPORATION_APPLICATION,
-  'Amalgamation Application' = CorpTypes.AMALGAMATION_APPLICATION,
-  Registration = CorpTypes.REGISTRATION,
-  'Continuation Application' = CorpTypes.CONTINUE_IN
+export const DisplayNameToCorpType: Record<string, string> = {
+  // Generate the mappings from the CorpTypes enum codes
+  [GetCorpFullDescription(CorpTypeCd.BC_COMPANY)]: CorpTypeCd.BC_COMPANY,
+  [GetCorpFullDescription(CorpTypeCd.BENEFIT_COMPANY)]: CorpTypeCd.BENEFIT_COMPANY,
+  [GetCorpFullDescription(CorpTypeCd.BC_CCC)]: CorpTypeCd.BC_CCC,
+  [GetCorpFullDescription(CorpTypeCd.BC_ULC_COMPANY)]: CorpTypeCd.BC_ULC_COMPANY,
+  [GetCorpFullDescription(CorpTypeCd.COOP)]: CorpTypeCd.COOP,
+  [GetCorpFullDescription(CorpTypeCd.PARTNERSHIP)]: CorpTypeCd.PARTNERSHIP,
+  [GetCorpFullDescription(CorpTypeCd.SOLE_PROP)]: CorpTypeCd.SOLE_PROP,
+
+  // Special cases that need manual mapping
+  'Name Request': CorpTypes.NAME_REQUEST,
+  'Incorporation Application': CorpTypes.INCORPORATION_APPLICATION,
+  'Amalgamation Application': CorpTypes.AMALGAMATION_APPLICATION,
+  Registration: CorpTypes.REGISTRATION,
+  'Continuation Application': CorpTypes.CONTINUE_IN
 }
