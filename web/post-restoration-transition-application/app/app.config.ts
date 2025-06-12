93552c3b6409e1f8324b3e0d6a133e024aff74ba
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-06-12 10:57:51 -0700

    feat: setting up post-restoration-transition-application

---
+++ web/post-restoration-transition-application/app/app.config.ts
@@ -0,0 +1,6 @@
+/* eslint-disable max-len */
+
+export default defineAppConfig({
+  // extend from the person-roles app config
+  // here add only specific overrides or new keys needed
+})
---
+++ web/post-restoration-transition-application/i18n/locales/en-CA.ts
@@ -0,0 +1,4 @@
+/* eslint-disable max-len */
+// we are also extending person-roles i18n lang files
+export default {
+}
