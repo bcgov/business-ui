b499ba938160105cc045f0c6ed9697860bb4a9eb
deetz99 <dietrich@wolpert.ca>
2025-06-24 15:25:48 -0700

    officer payload formatter

---
+++ web/person-roles/app/utils/format-officer.ts
@@ -0,0 +1,61 @@
+const ROLE_RELATIONSHIPS: [OfficerRole, string][] = [
+  [OfficerRole.CEO, 'CEO'],
+  [OfficerRole.CFO, 'CFO'],
+  [OfficerRole.PRESIDENT, 'President'],
+  [OfficerRole.VP, 'Vice President'],
+  [OfficerRole.CHAIR, 'Chair'],
+  [OfficerRole.TREASURER, 'Treasurer'],
+  [OfficerRole.SECRETARY, 'Secretary'],
+  [OfficerRole.ASSISTANT_SECRETARY, 'Assistant Secretary'],
+  [OfficerRole.OTHER, 'Other']
+]
+
+export const UI_ROLE_TO_API_ROLE_MAP = Object.fromEntries(ROLE_RELATIONSHIPS)
+
+export const API_ROLE_TO_UI_ROLE_MAP = Object.fromEntries(
+  ROLE_RELATIONSHIPS.map(([uiValue, apiValue]) => [apiValue.toLowerCase(), uiValue])
+)
+
+export function formatOfficerPayload(newState: OfficerTableState[]) {
+  const payload = newState.map((s) => {
+    const officer = s.state.officer
+
+    const roles = officer.roles.map((r) => {
+      return {
+        ...r,
+        roleType: UI_ROLE_TO_API_ROLE_MAP[r.roleType]
+      }
+    })
+
+    return {
+      entity: {
+        identifier: officer.id,
+        givenName: officer.firstName,
+        familyName: officer.lastName,
+        alternateName: officer.preferredName,
+        middleInitial: officer.middleName
+      },
+      mailingAddress: {
+        streetAddress: officer.mailingAddress.street,
+        streetAddressAdditional: officer.mailingAddress.streetAdditional,
+        addressCity: officer.mailingAddress.city,
+        addressCountry: officer.mailingAddress.country,
+        addressRegion: officer.mailingAddress.region,
+        postalCode: officer.mailingAddress.postalCode,
+        deliveryInstructions: officer.mailingAddress.locationDescription
+      },
+      deliveryAddress: {
+        streetAddress: officer.deliveryAddress.street,
+        streetAddressAdditional: officer.deliveryAddress.streetAdditional,
+        addressCity: officer.deliveryAddress.city,
+        addressCountry: officer.deliveryAddress.country,
+        addressRegion: officer.deliveryAddress.region,
+        postalCode: officer.deliveryAddress.postalCode,
+        deliveryInstructions: officer.deliveryAddress.locationDescription
+      },
+      roles
+    }
+  })
+
+  return payload
+}
