23ff1c7a08b6d952bcef189a8626179cdea7d71b
deetz99 <dietrich@wolpert.ca>
2025-06-26 15:34:05 -0700

    refactor modals to use common pattern

--- web/person-roles/app/composables/useModals.ts
+++ web/person-roles/app/composables/useModals.ts
@@ -1,40 +1,11 @@
 import type { ModalButtonProps } from '~/interfaces'
 import {
   ModalBase,
-  ModalOfficerInitializeError,
-  ModalOfficerFilingNotAllowed,
-  ModalOfficerSubmitError
+  ModalBaseError
 } from '#components'
 
 export const useModal = () => {
   const overlay = useOverlay()
-
-  async function openOfficerInitErrorModal(status: number | undefined) {
-    const modal = overlay.create(ModalOfficerInitializeError, {
-      props: {
-        status
-      }
-    })
-
-    await modal.open()
-  }
-
-  async function openOfficerFilingNotAllowedModal() {
-    const modal = overlay.create(ModalOfficerFilingNotAllowed)
-
-    await modal.open()
-  }
-
-  async function openOfficerSubmitErrorModal(status: number | undefined) {
-    const modal = overlay.create(ModalOfficerSubmitError, {
-      props: {
-        status,
-        i18nPrefix: 'error.submitFiling'
-      }
-    })
-
-    await modal.open()
-  }
 
   async function openBaseModal(title: string, description: string, dismissible: boolean, buttons: ModalButtonProps[]) {
     const modal = overlay.create(ModalBase, {
@@ -51,10 +22,22 @@ export const useModal = () => {
     return modal
   }
 
+  async function openBaseErrorModal(status: number | undefined, i18nPrefix: string, buttons?: ModalButtonProps[]) {
+    const modal = overlay.create(ModalBaseError, {
+      props: {
+        status,
+        i18nPrefix,
+        buttons
+      }
+    })
+
+    await modal.open()
+
+    return modal
+  }
+
   return {
-    openOfficerInitErrorModal,
-    openOfficerFilingNotAllowedModal,
-    openOfficerSubmitErrorModal,
-    openBaseModal
+    openBaseModal,
+    openBaseErrorModal
   }
 }
