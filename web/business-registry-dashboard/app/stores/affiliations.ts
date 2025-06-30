d42513f7c70c6d639674a01773891d8395953077
Rajandeep <rajandeep.kaur@gov.bc.ca>
2025-06-30 15:58:32 -0700

    29159 - NR Showing For Temps

--- web/business-registry-dashboard/app/stores/affiliations.ts
+++ web/business-registry-dashboard/app/stores/affiliations.ts
@@ -60,13 +60,11 @@ export const useAffiliationsStore = defineStore('brd-affiliations-store', () => 
 
   // Flag for whether server-side filtering is enabled
   const enableServerFiltering = computed(() =>
-    ldStore.getStoredFlag(LDFlags.EnableAffiliationsServerFiltering) || false
-  )
+true  )
 
   // Flag for whether pagination is enabled
   const enablePagination = computed(() =>
-    ldStore.getStoredFlag(LDFlags.EnableAffiliationsPagination) || false
-  )
+true  )
 
   const newlyAddedIdentifier = ref<string>('')
 
@@ -279,12 +277,14 @@ export const useAffiliationsStore = defineStore('brd-affiliations-store', () => 
       let affiliatedEntities: Business[] = []
 
       if (response.entities.length > 0) {
+        console.log("in responses entities", response.entities)
         response.entities.forEach((resp) => {
           const entity: Business = buildBusinessObject(resp)
           if (resp.nameRequest) {
+            console.log("in responses entities nameRequest", entity.nrNumber)
             const nr = resp.nameRequest
             if (!entity.nrNumber && nr.nrNum) {
-              entity.nrNumber = entity.nrNumber || nr.nrNum
+              entity.nrNumber = "test"
             }
             entity.nameRequest = buildNameRequestObject(nr)
           }
