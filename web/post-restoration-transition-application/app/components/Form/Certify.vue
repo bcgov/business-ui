7a3941fe38038dd60d190a0c4f2ba1975b281fb4
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-07-03 12:14:50 -0700

    style: update styling and how we track data-cy attribute

--- web/post-restoration-transition-application/app/components/Form/Certify.vue
+++ web/post-restoration-transition-application/app/components/Form/Certify.vue
@@ -27,8 +27,7 @@ const displayLegalName = computed(() => {
       <template #label>
         <div class="flex flex-col text-base space-y-2">
           <div :class="{ 'text-red-500': hasError }">
-            <span>{{ displayLegalName-5 }}</span>
-            <p>{{ $t('text.certifiesItHasRelevantKnowledge', { legalName: displayLegalName }) }}</p>
+            <p><span class="font-bold">{{ displayLegalName }}</span> {{ $t('text.certifiesItHasRelevantKnowledge', { legalName: displayLegalName }) }}</p>
           </div>
           <div>
             <span class="font-bold">{{ $t('label.date') }}: </span>{{ nowAsIsoDate() }}
