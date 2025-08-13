c4eaefbd819d6fb2f71487f45d0839fb596397b4
bobdev-94 <bobbyjin935@gmail.com>
2025-08-13 13:05:41 -0700

    29888 update_header_filter (#190)
    * 29888 update_header_filter
    
    * remove reset the filter value

--- web/business-registry-dashboard/app/stores/affiliations.ts
+++ web/business-registry-dashboard/app/stores/affiliations.ts
@@ -333,13 +333,6 @@ export const useAffiliationsStore = defineStore('brd-affiliations-store', () => 
       if (filtersChangedDuringLoading.value) {
         filtersChangedDuringLoading.value = false
 
-        // Update the actual filters with the latest values
-        if (affiliations.filters.businessName !== latestFilters.businessName) {
-          affiliations.filters.businessName = latestFilters.businessName
-        }
-        if (affiliations.filters.businessNumber !== latestFilters.businessNumber) {
-          affiliations.filters.businessNumber = latestFilters.businessNumber
-        }
         // Update type and status filters
         if (!isEqual(affiliations.filters.type, latestFilters.type)) {
           affiliations.filters.type = [...latestFilters.type]
@@ -463,7 +456,7 @@ export const useAffiliationsStore = defineStore('brd-affiliations-store', () => 
         }
       }
     },
-    { debounce: 400 } // 400ms debounce time - wait for user input to settle
+    { debounce: 500 } // 500ms debounce time - wait for user input to settle
   )
 
   // Separate watch for immediately resetting page when limit changes
