a228a3c761e86c400351ff000578d9b50220845f
deetz99 <dietrich@wolpert.ca>
2025-09-02 17:24:36 -0700

    fix lint

--- web/person-roles/app/stores/officer.ts
+++ web/person-roles/app/stores/officer.ts
@@ -1,7 +1,6 @@
 import type { ExpandedState, Row } from '@tanstack/vue-table'
 import { isEqual } from 'es-toolkit'
 import { isEmpty } from 'es-toolkit/compat'
-import { FetchError } from 'ofetch'
 
 export const useOfficerStore = defineStore('officer-store', () => {
   const na = useNuxtApp()
