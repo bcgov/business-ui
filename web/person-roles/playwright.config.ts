1bf89ac2c3fd6e1c04b6d006c68c943ff07d24f1
deetz99 <dietrich@wolpert.ca>
2025-09-19 10:17:30 -0700

    rework officers to deploy

--- web/person-roles/playwright.config.ts
+++ web/person-roles/playwright.config.ts
@@ -1,3 +1,4 @@
-import playwrightConfig from '@sbc-connect/playwright-config'
+// import playwrightConfig from '@sbc-connect/playwright-config'
+import playwrightConfig from '../../packages/configs/playwright-config/playwright.config'
 
 export default playwrightConfig
--- web/person-roles/vitest.config.ts
+++ web/person-roles/vitest.config.ts
@@ -1,3 +1,4 @@
-import vitestConfig from '@sbc-connect/vitest-config'
+// import vitestConfig from '@sbc-connect/vitest-config'
+import vitestConfig from '../../packages/configs/vitest-config'
 
 export default vitestConfig
