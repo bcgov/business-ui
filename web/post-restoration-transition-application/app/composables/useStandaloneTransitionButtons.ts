110b6bd2ea88095fce5346390b9f8c0c50513241
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-09-25 10:37:11 -0700

    lint

--- web/post-restoration-transition-application/app/composables/useStandaloneTransitionButtons.ts
+++ web/post-restoration-transition-application/app/composables/useStandaloneTransitionButtons.ts
@@ -62,7 +62,7 @@ export const useStandaloneTransitionButtons = () => {
           delete shareClassesData[i].removed
           delete shareClassesData[i].parentShareIndex
           if (shareClassesData[i].series) {
-            for (let j=0; j<shareClassesData[i].series.length; j++) {
+            for (let j = 0; j < shareClassesData[i].series.length; j++) {
               if (shareClassesData[i].series[j].removed === true) {
                 shareClassesData[i].series.splice(j, 1)
               } else {
