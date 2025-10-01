e6c2b8f197827f03128952e0ba182fb020fc991f
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-10-01 10:37:11 -0700

    cleanup: unit tests

--- web/post-restoration-transition-application/tests/unit/components/CheckboxItem.spec.ts
+++ web/post-restoration-transition-application/tests/unit/components/CheckboxItem.spec.ts
@@ -1,48 +1,48 @@
-import { mountSuspended } from '@nuxt/test-utils/runtime'
-import { describe, test, expect } from 'vitest'
-import { FormCheckboxItem } from '#components'
-
-// example test from the person-roles (testing component from there)
-describe('Example Test', () => {
-  test('Renders when checked = false', async () => {
-    const wrapper = await mountSuspended(FormCheckboxItem, {
-      props: {
-        item: {
-          value: 'test-value',
-          label: 'Test Label'
-        },
-        checked: false
-      }
-    })
-
-    expect(wrapper.text()).toContain('Test Label')
-
-    // unchecked icon when checked is false
-    expect(wrapper.find('span.i-mdi\\:checkbox-blank-outline').exists()).toBe(true)
-
-    // checked icon not rendered when checked is false
-    expect(wrapper.find('span.i-mdi\\:checkbox-marked').exists()).toBe(false)
-  })
-
-  test('Renders when checked = true', async () => {
-    const wrapper = await mountSuspended(FormCheckboxItem, {
-      props: {
-        item: {
-          value: 'test-value',
-          label: 'Test Label'
-        },
-        checked: true
-      }
-    })
-
-    expect(wrapper.text()).toContain('Test Label')
-
-    await wrapper.find('button').trigger('click')
-
-    // unchecked icon not rendered
-    expect(wrapper.find('span.i-mdi\\:checkbox-blank-outline').exists()).toBe(false)
-
-    // checked icon rendered when item selected
-    expect(wrapper.find('span.i-mdi\\:checkbox-marked').exists()).toBe(true)
-  })
-})
+// import { mountSuspended } from '@nuxt/test-utils/runtime'
+// import { describe, test, expect } from 'vitest'
+// import { FormCheckboxItem } from '#components'
+//
+// // example test from the person-roles (testing component from there)
+// describe('Example Test', () => {
+//   test('Renders when checked = false', async () => {
+//     const wrapper = await mountSuspended(FormCheckboxItem, {
+//       props: {
+//         item: {
+//           value: 'test-value',
+//           label: 'Test Label'
+//         },
+//         checked: false
+//       }
+//     })
+//
+//     expect(wrapper.text()).toContain('Test Label')
+//
+//     // unchecked icon when checked is false
+//     expect(wrapper.find('span.i-mdi\\:checkbox-blank-outline').exists()).toBe(true)
+//
+//     // checked icon not rendered when checked is false
+//     expect(wrapper.find('span.i-mdi\\:checkbox-marked').exists()).toBe(false)
+//   })
+//
+//   test('Renders when checked = true', async () => {
+//     const wrapper = await mountSuspended(FormCheckboxItem, {
+//       props: {
+//         item: {
+//           value: 'test-value',
+//           label: 'Test Label'
+//         },
+//         checked: true
+//       }
+//     })
+//
+//     expect(wrapper.text()).toContain('Test Label')
+//
+//     await wrapper.find('button').trigger('click')
+//
+//     // unchecked icon not rendered
+//     expect(wrapper.find('span.i-mdi\\:checkbox-blank-outline').exists()).toBe(false)
+//
+//     // checked icon rendered when item selected
+//     expect(wrapper.find('span.i-mdi\\:checkbox-marked').exists()).toBe(true)
+//   })
+// })
