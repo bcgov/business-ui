2571fdf733f04765ffa6a3381df1adc3244be8ac
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-10-08 15:14:15 -0700

    pr feedback

--- web/post-restoration-transition-application/app/utils/compare.ts
+++ web/post-restoration-transition-application/app/utils/compare.ts
@@ -1,13 +1,13 @@
 export const compare = function (a: object, b: object): boolean {
   const keysToCompare = Object.keys({ ...a, ...b })
     .filter(key => ![null, undefined]
-    .includes(a[key]) || ![null, undefined].includes(b[key]))
+    .includes(a[key]) || ![null, undefined, ''].includes(b[key]))
   let rv = true
-  for (let i = 0; i < keysToCompare.length; i++) {
-    if (typeof a[keysToCompare[i]] === 'object') {
-      rv = rv && compare(a[keysToCompare[i]], b[keysToCompare[i]])
+  for (const key of keysToCompare) {
+    if (typeof a[key] === 'object') {
+      rv = rv && compare(a[key], b[key])
     } else {
-      rv = rv && a[keysToCompare[i]] === b[keysToCompare[i]]
+      rv = rv && a[key] === b[key]
     }
     if (!rv) {
       break
