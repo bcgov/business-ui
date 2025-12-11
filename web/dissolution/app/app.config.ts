69239188930841a9979bfb3952da5225bcf003fe
deetz99 <dietrich@wolpert.ca>
2025-12-11 12:37:41 -0800

    add missing app config and test setup files

---
+++ web/dissolution/app/app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  connect: {
+    header: {
+      localeSelect: false
+    }
+  }
+})
---
+++ web/dissolution/tests/unit/setup.ts
@@ -0,0 +1,5 @@
+// Vitest/Nuxt provides this already, no need to add plugin
+// import { config } from '@vue/test-utils'
+// import { enI18n as i18nMock } from '~~/tests/mocks/i18n'
+
+// config.global.plugins.push(i18nMock)
