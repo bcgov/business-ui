51b0f1d0ec6a8fdcd048833729ecec7f85225bb9
deetz99 <dietrich@wolpert.ca>
2026-01-06 15:07:39 -0800

    fix test name

--- packages/layers/base/tests/unit/composables/useBusinessAddresses.spec.ts
+++ packages/layers/base/tests/unit/composables/useBusinessAddresses.spec.ts
@@ -10,51 +10,45 @@ const mockBusinessApi = {
 mockNuxtImport('useBusinessApi', () => () => mockBusinessApi)
 
 describe('useBusinessAddresses', () => {
+  const addressesMock = getBusinessAddressesMock()
+
   beforeEach(async () => {
     vi.resetAllMocks()
+    mockBusinessApi.getBusinessAddresses.mockResolvedValue(
+      {
+        data: { value: addressesMock },
+        error: { value: undefined },
+        status: { value: 'success' },
+        refresh: async () => ({ data: addressesMock })
+      }
+    )
   })
 
-  describe('initFiling', () => {
-    const addressesMock = getBusinessAddressesMock()
+  describe('getBusinessAddresses', () => {
+    test('should format the response to the expected UiEntityOfficeAddress format', async () => {
+      const resp = await useBusinessAddresses().getBusinessAddresses(identifier)
 
-    beforeEach(async () => {
-      vi.resetAllMocks()
-      mockBusinessApi.getBusinessAddresses.mockResolvedValue(
-        {
-          data: { value: addressesMock },
-          error: { value: undefined },
-          status: { value: 'success' },
-          refresh: async () => ({ data: addressesMock })
-        }
-      )
-    })
+      expect(mockBusinessApi.getBusinessAddresses).toHaveBeenCalled()
+      // mock does not include businessOffice
+      expect(resp.data?.businessOffice).toBeUndefined()
 
-    describe('getBusinessAddresses', () => {
-      test('should format the response to the expected UiEntityOfficeAddress format', async () => {
-        const resp = await useBusinessAddresses().getBusinessAddresses(identifier)
+      // should format liquidationRecordsOffice
+      expect(resp.data?.liquidationRecordsOffice).toBeDefined()
+      expect(resp.data?.liquidationRecordsOffice?.deliveryAddress.street)
+        .toEqual(addressesMock.liquidationRecordsOffice?.deliveryAddress?.streetAddress)
+      expect(resp.data?.liquidationRecordsOffice?.sameAs).toBe(false)
 
-        expect(mockBusinessApi.getBusinessAddresses).toHaveBeenCalled()
-        // mock does not include businessOffice
-        expect(resp.data?.businessOffice).toBeUndefined()
+      // should format recordsOffice
+      expect(resp.data?.recordsOffice).toBeDefined()
+      expect(resp.data?.recordsOffice?.deliveryAddress.street)
+        .toEqual(addressesMock.recordsOffice?.deliveryAddress?.streetAddress)
+      expect(resp.data?.recordsOffice?.sameAs).toBe(true)
 
-        // should format liquidationRecordsOffice
-        expect(resp.data?.liquidationRecordsOffice).toBeDefined()
-        expect(resp.data?.liquidationRecordsOffice?.deliveryAddress.street)
-          .toEqual(addressesMock.liquidationRecordsOffice?.deliveryAddress?.streetAddress)
-        expect(resp.data?.liquidationRecordsOffice?.sameAs).toBe(false)
-
-        // should format recordsOffice
-        expect(resp.data?.recordsOffice).toBeDefined()
-        expect(resp.data?.recordsOffice?.deliveryAddress.street)
-          .toEqual(addressesMock.recordsOffice?.deliveryAddress?.streetAddress)
-        expect(resp.data?.recordsOffice?.sameAs).toBe(true)
-
-        // should format registeredOffice
-        expect(resp.data?.registeredOffice).toBeDefined()
-        expect(resp.data?.registeredOffice?.deliveryAddress.street)
-          .toEqual(addressesMock.registeredOffice?.deliveryAddress?.streetAddress)
-        expect(resp.data?.registeredOffice?.sameAs).toBe(true)
-      })
+      // should format registeredOffice
+      expect(resp.data?.registeredOffice).toBeDefined()
+      expect(resp.data?.registeredOffice?.deliveryAddress.street)
+        .toEqual(addressesMock.registeredOffice?.deliveryAddress?.streetAddress)
+      expect(resp.data?.registeredOffice?.sameAs).toBe(true)
     })
   })
 })
