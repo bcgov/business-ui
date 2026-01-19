a4b7d13324be033b895efefe390b16ee86078049
Kial Jinnah <kialj876@gmail.com>
2026-01-19 13:51:59 -0500

    cleanup
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- packages/layers/base/app/services/helpers/get-cached-or-fetch.ts
+++ packages/layers/base/app/services/helpers/get-cached-or-fetch.ts
@@ -40,40 +40,4 @@ export async function getCachedOrFetch<TData>(
     : await cache.refresh(entry)
 
   return result.data!
-
-  // // NB: _localPromise
-  // // required for concurrent requests triggering the same query (eg: Promise.all)
-  // // where entry.pending hasn't updated yet (<1ms difference)
-
-  // // ensure new request if force
-  // if (force && entry.meta._localPromise) {
-  //   delete entry.meta._localPromise
-  // }
-  // // returns existing promise if !force and a request is in progress
-  // if (!force && entry.meta._localPromise) {
-  //   const result = await (entry.meta._localPromise as Promise<DataState<TData, Error>>)
-  //   return result.data!
-  // }
-
-  // // fetch always triggers a network call
-  // // refresh will return the cached data unless the entry is marked as stale or entry.status === 'error'
-  // // - in which case it will trigger a network call
-  // // both methods will propagate the error if the network call fails
-  // const promise = force
-  //   ? cache.fetch(entry)
-  //   : cache.refresh(entry)
-
-  // // assign promise reference for concurrent request check
-  // entry.meta._localPromise = promise
-
-  // try {
-  //   const result = await (promise as Promise<DataState<TData, Error>>)
-  //   // assume valid data if no error thrown
-  //   return result.data!
-  // } finally {
-  //   // cleanup for future requests
-  //   if (entry.meta._localPromise === promise) {
-  //     delete entry.meta._localPromise
-  //   }
-  // }
 }
