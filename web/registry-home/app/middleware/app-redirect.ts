792f246415c628c07e6e3eb36270fcbab68fc09f
Cameron Bowler <cameron.bowler@ca.ey.com>
2026-01-26 14:00:19 -0800

    redirect to account select when populuate is preset. version bump

--- web/registry-home/app/middleware/app-redirect.ts
+++ web/registry-home/app/middleware/app-redirect.ts
@@ -1,15 +1,20 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to) => {
   const config = useRuntimeConfig().public
+  const localePath = useLocalePath()
 
   // ToDo: Can remove this middleware once BRH is setup
-  return navigateTo(
-    {
-      path: appendUrlParam(
-        config.brdUrl,
-        'accountid',
-        useConnectAccountStore().currentAccount.id
-      )
-    },
-    { external: true }
-  )
+  if (to.query?.populate) {
+    return navigateTo({ path: localePath('/auth/account/select'), query: to.query })
+  } else {
+    return navigateTo(
+      {
+        path: appendUrlParam(
+          config.brdUrl,
+          'accountid',
+          useConnectAccountStore().currentAccount.id
+        )
+      },
+      { external: true }
+    )
+  }
 })
