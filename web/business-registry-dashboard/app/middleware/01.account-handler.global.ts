f9e5aeb8474e546ad467550a90c9d4b82f2c80b2
Cameron Bowler <cameron.bowler@ca.ey.com>
2026-01-27 16:21:01 -0800

    include CP regex recommendation

--- web/business-registry-dashboard/app/middleware/01.account-handler.global.ts
+++ web/business-registry-dashboard/app/middleware/01.account-handler.global.ts
@@ -1,8 +1,9 @@
 export default defineNuxtRouteMiddleware((to) => {
   const localePath = useLocalePath()
 
-  // Matches /account/<id> anywhere in the path (including locale prefixes)
-  const hasAccountInPath = /(^|\/)account\/[^/]+/.test(to.path)
+  // Matches /account/<id> anywhere in the path (including locale prefixes),
+  // ensuring the account ID segment is followed only by "/" or the end
+  const hasAccountInPath = /(^|\/)account\/[^/]+(\/|$)/.test(to.path)
   if (hasAccountInPath) { return }
 
   if (to.query.accountid) {
