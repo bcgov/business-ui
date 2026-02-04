6ad7a97a8a6fd862a8d1a40666e1de69bab1aca3
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2026-02-03 18:12:13 -0800

    Correct Account Handler Middleware (#377)
    * Correct middleware to preserve the magic link functionality
    
    * comment update
    
    * remove redundant code

--- web/business-registry-dashboard/app/middleware/01.account-handler.global.ts
+++ web/business-registry-dashboard/app/middleware/01.account-handler.global.ts
@@ -1,12 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
   const localePath = useLocalePath()
 
-  // Matches /account/<id> anywhere in the path (including locale prefixes),
-  // ensuring the account ID segment is followed only by "/" or the end
-  const hasAccountInPath = /(^|\/)account\/[^/]+(\/|$)/.test(to.path)
-  if (hasAccountInPath) { return }
-
-  if (to.query.accountid) {
+  // If accountid and populate are in the query params, redirect to /account/<id>
+  if (to.query.accountid && to.query.populate) {
     // Pull accountid out, keep the rest of the query params
     const { accountid, ...restQuery } = to.query
 
