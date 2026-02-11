456cf489c157ca50f7d4a97c10f7754bcefb0225
deetz99 <dietrich@wolpert.ca>
2026-02-11 14:05:36 -0800

    initial share series form

--- packages/layers/base/app/components/Form/Share/Series/index.vue
+++ packages/layers/base/app/components/Form/Share/Series/index.vue
@@ -18,17 +18,16 @@ const { t } = useI18n()
 const { alerts, attachAlerts } = useFilingAlerts(props.stateKey)
 const { tableState } = useManageShareStructure(props.stateKey)
 const formTarget = 'party-details-form'
-const currencyOptions = getCurrencyList()
 const schema = computed(() => {
   const currentId = model.value.id
   const nameList = tableState.value
     .filter(item => item.new.id !== currentId)
     .map(item => item.new.name.toLowerCase())
-  return getActiveShareClassSchema(nameList)
+  return getActiveShareSeriesSchema(nameList)
 })
 
-const model = defineModel<ShareClassSchema>({ required: true })
-const formRef = useTemplateRef<Form<ShareClassSchema>>('share-class-form')
+const model = defineModel<ShareSeriesSchema>({ required: true })
+const formRef = useTemplateRef<Form<ShareSeriesSchema>>('share-series-form')
 
 async function onDone() {
   try {
@@ -41,12 +40,12 @@ async function onDone() {
 
 const { targetId, messageId } = attachAlerts(formTarget, model)
 
-provide('UInput-slots-share-class-name-input', { trailing: h('span', { class: 'text-base font-bold' }, 'Shares') })
+provide('UInput-slots-share-series-name-input', { trailing: h('span', { class: 'text-base font-bold' }, 'Shares') })
 </script>
 
 <template>
   <UForm
-    ref="share-class-form"
+    ref="share-series-form"
     :schema
     :name
     :nested
@@ -69,10 +68,10 @@ provide('UInput-slots-share-class-name-input', { trailing: h('span', { class: 't
         <ConnectFormInput
           v-model="model.name"
           required
-          :label="'Class Name [Shares]'"
-          input-id="share-class-name-input"
+          :label="'Series Name [Shares]'"
+          input-id="share-series-name-input"
           name="name"
-          help="Only enter the class name. The word “shares” will be added automatically."
+          help="Only enter the series name. The word “Shares” will be added automatically."
         />
         <USeparator />
         <URadioGroup
@@ -101,51 +100,7 @@ provide('UInput-slots-share-class-name-input', { trailing: h('span', { class: 't
           </template>
         </URadioGroup>
         <USeparator />
-        <URadioGroup
-          v-model="model.hasParValue"
-          size="xl"
-          :items="[{ value: true }, { label: 'No Par Value', value: false }]"
-          :ui="{
-            fieldset: 'gap-y-6',
-            item: 'items-center gap-4'
-          }"
-          @update:model-value="model.parValue = null, model.currency = undefined"
-        >
-          <template #label="{ item }">
-            <div v-if="item.value" class="flex flex-col gap-2 sm:gap-4 sm:flex-row">
-              <ConnectFormInput
-                v-model.trim="model.parValue"
-                :disabled="!model.hasParValue"
-                :class="{ 'opacity-75': !model.hasParValue }"
-                input-id="par-value-input"
-                :label="'Par Value'"
-                name="parValue"
-                :required="model.hasParValue"
-                help="Initial value of each share"
-                class="w-full flex-1"
-              />
-              <UFormField
-                name="currency"
-                help="Initial value of each share"
-                data-testid="form-field-currency"
-                :class="{ 'opacity-75': !model.hasParValue }"
-                class="w-full flex-1"
-              >
-                <ConnectInputMenu
-                  id="par-value-currency-input"
-                  v-model="model.currency"
-                  label="Currency"
-                  :items="currencyOptions"
-                  class="w-full"
-                  :required="model.hasParValue"
-                  :disabled="!model.hasParValue"
-                  open-on-focus
-                />
-              </UFormField>
-            </div>
-            <span v-else>{{ item.label }}</span>
-          </template>
-        </URadioGroup>
+        <span>No Par Value</span>
         <USeparator />
         <UFormField name="hasRightsOrRestrictions">
           <UCheckbox
