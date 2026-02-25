fbb0e4eed5dc45ace40032fbe2aa6b5edbcf7790
Kial Jinnah <kialj876@gmail.com>
2026-02-25 15:08:52 -0500

    chore: comment update
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- web/registry-home/app/middleware/app-redirect.ts
+++ web/registry-home/app/middleware/app-redirect.ts
@@ -9,7 +9,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   // FUTURE: Temporary fallback until the new BRH home page exists—migration flows
   // use ?token= to enter extended auth, but non-migration traffic is routed to BRD for now.
   if (to.query?.token) {
-    // FUTURE: ticket number - update in magic auth link after email ticket is done
+    // FUTURE: #32612 - update in magic auth link after email ticket is done
     to.query.return = config.brdUrl + 'affiliationInvitation/token'
     return navigateTo({ path: localePath('/auth/account/select'), query: to.query })
   } else {
