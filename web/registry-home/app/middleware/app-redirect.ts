48c0dc487a3396c58462984d82d2df1260155191
Kial Jinnah <kialj876@gmail.com>
2026-02-25 15:42:34 -0500

    chore: fix redirect path
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- web/registry-home/app/middleware/app-redirect.ts
+++ web/registry-home/app/middleware/app-redirect.ts
@@ -10,7 +10,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   // use ?token= to enter extended auth, but non-migration traffic is routed to BRD for now.
   if (to.query?.token) {
     // FUTURE: #32612 - update in magic auth link after email ticket is done
-    to.query.return = config.brdUrl + 'affiliationInvitation/token'
+    to.query.return = config.brdUrl + 'affiliationInvitation/acceptToken'
     return navigateTo({ path: localePath('/auth/account/select'), query: to.query })
   } else {
     return navigateTo(
