548a4001b725ff2c8637f4eb257aaa1c5437ae72
deetz99 <dietrich@wolpert.ca>
2026-03-20 11:31:07 -0700

    liquidator task guard e2e test

--- web/person-roles/tests/e2e/specs/receivers/task-guards.spec.ts
+++ web/person-roles/tests/e2e/specs/receivers/task-guards.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
-import { navigateToManageLiquidatorsPage } from '../../test-utils'
-import { LiquidateType } from '#business/app/enums/liquidate-type'
-import { getPartiesMock, getBusinessAddressesMock, mockCommonApiCallsForFiling } from '#test-mocks'
+import { navigateToManageReceiversPage } from '../../test-utils'
+import { ReceiverType } from '#business/app/enums/receiver-type'
+import { getPartiesMock, mockCommonApiCallsForFiling } from '#test-mocks'
 
 const identifier = 'BC1234567'
 
@@ -13,77 +13,57 @@ test.describe('Task Guards', () => {
       page,
       identifier,
       getPartiesMock([
-        { index: 0, key: 'roleType', value: 'Liquidator' },
-        { index: 1, key: 'roleType', value: 'Liquidator' },
-        { index: 2, key: 'roleType', value: 'Liquidator' }
+        { index: 0, key: 'roleType', value: 'Receiver' },
+        { index: 1, key: 'roleType', value: 'Receiver' },
+        { index: 2, key: 'roleType', value: 'Receiver' }
       ]),
       undefined,
-      getBusinessAddressesMock()
+      undefined
     )
 
-    await navigateToManageLiquidatorsPage(page, LiquidateType.ADDRESS)
+    await navigateToManageReceiversPage(page, ReceiverType.AMEND)
   })
 
-  test('should prevent save/submit when no changes have been made to liquidators or offices', async ({ page }) => {
+  test('should prevent save/submit when no changes have been made to receivers', async ({ page }) => {
     // select no fee to satisfy formState validation
     const staffNoFeeRadio = page.getByRole('radio', { name: 'No Fee' })
     expect(staffNoFeeRadio).toBeVisible()
     await staffNoFeeRadio.click()
 
     await page.getByRole('button', { name: 'Submit' }).click()
-    await expect(page.getByTestId('right-buttons')).toContainText('There are no changes to submit')
+    await expect(page.getByTestId('right-buttons')).toContainText('Update at least one Receiver to submit.')
 
     await page.getByRole('button', { name: 'Save and Resume Later' }).click()
     await expect(page.getByTestId('left-buttons')).toContainText('There are no changes to save')
   })
 
-  test('should prevent save/submit when liquidator or office sub form open', async ({ page }) => {
+  test('should prevent save/submit when receiver sub form open', async ({ page }) => {
     // select no fee to satisfy formState validation
     const staffNoFeeRadio = page.getByRole('radio', { name: 'No Fee' })
     expect(staffNoFeeRadio).toBeVisible()
     await staffNoFeeRadio.click()
 
-    const liqSection = page.getByTestId('liquidator-info-section')
-    const officeSection = page.getByTestId('records-office-section')
+    const partySection = page.getByTestId('manage-parties')
     const submitBtn = page.getByRole('button', { name: 'Submit' })
     const saveBtn = page.getByRole('button', { name: 'Save and Resume Later' })
-    const liqSubForm = page.getByRole('group', { name: 'Editing TESTER TESTING' })
-    const officeSubForm = page.getByRole('group', { name: 'Editing Liquidation Records Office' })
-    const taskMessage = 'Finish this task before making other changes.'
+    const partySubForm = page.getByRole('group', { name: 'Editing TESTER TESTING' })
 
-    // open liquidator sub form
-    await liqSection.getByRole('button', { name: 'Change' }).first().click()
+    // open party sub form
+    await partySection.getByRole('button', { name: 'Change' }).first().click()
     // try to submit
     await submitBtn.click()
     // assert task message
-    await expect(liqSubForm).toContainText(taskMessage)
+    await expect(partySubForm).toContainText('Finish this task before submitting.')
     // close sub form
-    await liqSubForm.getByRole('button', { name: 'Cancel' }).first().click()
+    await partySubForm.getByRole('button', { name: 'Cancel' }).first().click()
     // reopen sub form
-    await liqSection.getByRole('button', { name: 'Change' }).first().click()
+    await partySection.getByRole('button', { name: 'Change' }).first().click()
     // try to save
     await saveBtn.click()
     // assert task message
-    await expect(liqSubForm).toContainText(taskMessage)
+    await expect(partySubForm).toContainText('Finish this task before saving.')
     // close sub form
-    await liqSubForm.getByRole('button', { name: 'Cancel' }).first().click()
-
-    // open office sub form
-    await officeSection.getByRole('button', { name: 'Change' }).first().click()
-    // try to submit
-    await submitBtn.click()
-    // assert task message
-    await expect(officeSubForm).toContainText(taskMessage)
-    // close sub form
-    await officeSubForm.getByRole('button', { name: 'Cancel' }).first().click()
-    // reopen office sub form
-    await officeSection.getByRole('button', { name: 'Change' }).first().click()
-    // try to save
-    await saveBtn.click()
-    // assert task message
-    await expect(officeSubForm).toContainText(taskMessage)
-    // close sub form
-    await officeSubForm.getByRole('button', { name: 'Cancel' }).first().click()
+    await partySubForm.getByRole('button', { name: 'Cancel' }).first().click()
   })
 
   test('should be able to cancel when no changes have been made', async ({ page }) => {
@@ -94,7 +74,7 @@ test.describe('Task Guards', () => {
   })
 
   test('should display modal on cancel when changes have been made', async ({ page }) => {
-    await expect(page.getByTestId('liquidator-info-section')).toContainText('TESTER TESTING')
+    await expect(page.getByTestId('manage-parties')).toContainText('TESTER TESTING')
 
     const staffNoFeeRadio = page.getByRole('radio', { name: 'No Fee' })
     expect(staffNoFeeRadio).toBeVisible()
@@ -113,7 +93,7 @@ test.describe('Task Guards', () => {
   })
 
   test('should prevent navigation with browser popup if changes have been made', async ({ page }) => {
-    await expect(page.getByTestId('liquidator-info-section')).toContainText('TESTER TESTING')
+    await expect(page.getByTestId('manage-parties')).toContainText('TESTER TESTING')
 
     const staffNoFeeRadio = page.getByRole('radio', { name: 'No Fee' })
     expect(staffNoFeeRadio).toBeVisible()
@@ -129,7 +109,7 @@ test.describe('Task Guards', () => {
 
     // Try to navigate away with unsaved changes
     await page.getByRole('link', { name: 'MCELROY ENTERPRISES LTD. -' }).click({ delay: 500 })
-    // should still be on manage-liquidators page due to browser dialog
-    await expect(page).toHaveURL(/.*manage-liquidators.*/)
+    // should still be on manage-receivers page due to browser dialog
+    await expect(page).toHaveURL(/.*manage-receivers.*/)
   })
 })
