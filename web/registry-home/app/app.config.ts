0d1c1e15e28bc3cf18ba369e45fde4e5875f572e
Cameron Bowler <cameron.bowler@ca.ey.com>
2026-03-25 06:58:20 -0700

    Acct Flow Text Updates

--- web/registry-home/app/app.config.ts
+++ web/registry-home/app/app.config.ts
@@ -11,12 +11,7 @@ export default defineAppConfig({
     bcscUser: {
       login: {
         idps: ['bcsc'],
-        idpEnforcement: true,
-        alert: {
-          title: 'Welcome to the new Business Registry',
-          message: 'To complete the move of your business, sign in or create an account using your BC Services '
-            + 'Card below.'
-        }
+        idpEnforcement: true
       }
     }
   }
--- web/registry-home/i18n/locales/en-CA.ts
+++ web/registry-home/i18n/locales/en-CA.ts
@@ -4,5 +4,14 @@ export default {
       title: 'Home - BC Business Registry',
       h1: 'BC Business Registry Home'
     }
+  },
+  connect: {
+    page: {
+      login: {
+        h1: 'Log in to BC Registries',
+        description: 'In order to use this service, you’ll need one of the following logins. This login will be '
+          + 'linked to your BC Registries account for identity verification purposes.'
+      }
+    }
   }
 }
