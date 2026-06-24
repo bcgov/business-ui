ffa36560f63e8158ae79a366443b581d89ee4acd
deetz99 <73151365+deetz99@users.noreply.github.com>
2026-06-24 09:12:14 -0700

    update connect-pay, add page desc (#502)

--- web/registry-home/i18n/locales/en-CA.ts
+++ web/registry-home/i18n/locales/en-CA.ts
@@ -5,6 +5,9 @@ export default {
       sbcAccountCreation: 'Create a BC Registries Account'
     },
     page: {
+      createAccount: {
+        description: ['Create a new account to use your services and manage all of your businesses within BC Registries.']
+      },
       login: {
         h1: 'Log in to the BC Business Registry',
         description: 'You’ll need a BC Services Card account to use this service. Signing in will securely link your account to BC Registries and prove your identity online.'
