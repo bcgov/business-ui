67165662510e3a12097f2f041f23e582a780505a
deetz99 <dietrich@wolpert.ca>
2026-07-13 11:12:06 -0700

    create resolution dates table columns

---
+++ packages/layers/base/app/utils/business-table/columns/get-resolution-date-column.ts
@@ -0,0 +1,37 @@
+import { TableColumnIdentity } from '#components'
+import { h } from 'vue'
+import { merge } from 'es-toolkit'
+import { DELETED_CLASS } from './constants'
+
+export function getResolutionDateColumn<T extends ResolutionDateSchema>(
+  metaOption: TableColumnMetaOption = 'first',
+  badgeLabelOverrides?: Partial<Record<ActionType, string>>,
+  metaOverrides: Partial<TableBusinessColumnMeta<T>> = {}
+): TableBusinessColumn<T> {
+  const t = useNuxtApp().$i18n.t
+  const defaultMeta = getColumnMeta<T>(metaOption)!
+  const meta = merge(defaultMeta, metaOverrides)
+
+  const dateColumn: TableBusinessColumn<T> = {
+    id: 'resolution-date',
+    meta,
+    header: () => h('span', { class: 'sr-only' }, t('label.resolutionOrCourtOrderDate')),
+    cell: ({ row }) => {
+      const badges = getTableBadges(row, badgeLabelOverrides)
+      const isRemoved = getIsRowRemoved(row)
+      const defaultClass = 'w-full overflow-clip' // Set the width to 100% of the outer layer
+
+      return h(
+        TableColumnIdentity,
+        {
+          label: row.original.new.date,
+          badges,
+          class: defaultClass,
+          labelClass: isRemoved ? DELETED_CLASS : ''
+        }
+      )
+    }
+  }
+
+  return dateColumn
+}
