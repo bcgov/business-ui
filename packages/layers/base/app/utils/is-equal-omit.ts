022b510b6f500897e21eeba919e448a842ac3bc2
deetz99 <dietrich@wolpert.ca>
2026-08-13 11:47:26 -0700

    type fix

--- packages/layers/base/app/utils/is-equal-omit.ts
+++ packages/layers/base/app/utils/is-equal-omit.ts
@@ -6,11 +6,19 @@ import { isEqual, omit } from 'es-toolkit'
  * @param objA The first object to compare
  * @param objB The second object to compare
  * @param keys Keys to omit
+ * 
+ * @example
+ * const NON_EDITABLE_FIELDS = ['id', 'isEditing', 'actions'] as const
+ * 
+ * const subjectA = { id: '123', name: 'Court Order', isEditing: true }
+ * const subjectB = { id: '456', name: 'Court Order', isEditing: false }
+ * 
+ * isEqualOmit(subjectA, subjectB, NON_EDITABLE_FIELDS) // returns true
 */
 export function isEqualOmit<T extends object>(
   objA: T,
   objB: T,
-  keys: Array<keyof T>
+  keys: ReadonlyArray<keyof T>
 ): boolean {
   // null/undefined check
   if (!objA || !objB) {
