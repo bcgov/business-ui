d8147766564ef7141ab87bc68acaee5d93b0416d
deetz99 <dietrich@wolpert.ca>
2026-08-13 15:11:03 -0700

    create ManageCourtOrders component

--- packages/layers/base/.playground/app/pages/examples/components/ManageCourtOrders/index.vue
+++ packages/layers/base/.playground/app/pages/examples/components/ManageCourtOrders/index.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
+import { delay } from 'es-toolkit'
+import mockCourtOrders from '#test-mocks/business-court-orders/json/default.json'
+
 definePageMeta({
   layout: 'connect-auth',
   breadcrumbs: [{ label: 'Examples', to: '/' }, { label: 'Manage Court Orders' }]
 })
 
 const schema = getCourtOrderPoaFullSchema()
-const service = useBusinessService()
-const data = ref<TableBusinessState<CourtOrderPoaFullSchema>[]>([])
+const { tableState } = useManageCourtOrders()
+
+const activeCourtOrder = ref<ActiveCourtOrderPoaFullSchema | undefined>(undefined)
+const loading = ref(false)
 
 onMounted(async () => {
-  const courtOrders = await service.getCourtOrders('BC0878506')
-  const mapped = courtOrders.map((co) => {
-    const parsed = schema.parse(co)
-    return { old: structuredClone(parsed), new: structuredClone(parsed) }
-  })
-  data.value = mapped
+  try {
+    loading.value = true
+    await delay(1500)
+    const mapped = mockCourtOrders.courtOrders.map((co) => {
+      const parsed = schema.parse(co)
+      return { old: structuredClone(parsed), new: structuredClone(parsed) }
+    })
+    tableState.value = mapped
+  } catch {
+    // should never happen
+    console.error('Error initializing mock data')
+  } finally {
+    loading.value = false
+  }
 })
 </script>
 
@@ -24,7 +37,12 @@ onMounted(async () => {
       :heading="{ label: 'Manage Court Orders - Default' }"
       ui-body="p-10"
     >
-      <TableCourtOrder :data="data" />
+      <ManageCourtOrders
+        v-model:active-co="activeCourtOrder"
+        :loading
+        subject="Court Order"
+        table-title="Court Orders/Plan of Arrangement"
+      />
     </ConnectPageSection>
   </UContainer>
 </template>
